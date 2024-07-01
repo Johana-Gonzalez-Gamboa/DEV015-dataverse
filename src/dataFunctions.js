3d7881c3809618562cf3755360c86b3f9e9e37e4
noir.desir.sublime@gmail.com <tuemailexample.com>
2024-07-01 18:47:21 -0400

    Avance para filtrado

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,19 @@
-// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
+export function filterData(data, filterBy, value) {
+  // Verificar que la función está recibiendo los valores
+  console.log('Data:', data);
+  console.log('Filter By:', filterBy);
+  console.log('Value:', value);
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return [];
-};
+  // Filtrar los datos según el campo y el valor proporcionados
+  return data.filter(item => {
+    // Navegar a través del objeto para llegar al campo especificado
+    const keys = filterBy.split('.');
+    let fieldValue = item;
+    for (const key of keys) {
+      fieldValue = fieldValue[key];
+      if (!fieldValue) break;
+    }
+    console.log('Field Value:', fieldValue); // <-- Añade esta línea para imprimir el valor del campo
+    return fieldValue === value;
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//import { example } from './dataFunctions.js';
+import { filterData } from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
@@ -11,3 +11,14 @@ function init() {
 }
 // Ejecutar la función de inicialización cuando el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', init);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const select = document.getElementById('sortBy'); // Seleccionar el elemento <select> por su id
+  select.addEventListener('change', function() { // Agregar un evento 'change' al <select>
+    const selectedValue = select.value; // es pa obtener el valor seleccionado del <select>
+    const filterBy = 'extraInfo.tipoGuardian'; // El campo por el que filtramos
+    const filteredData = filterData(data, filterBy, selectedValue); // es una variable que captura el resultado del proceso de filtrado de datos 
+    console.log('Datos filtrados:', filteredData);
+
+  });
+});
